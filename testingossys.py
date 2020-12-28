61c1751724de716bbce7660121c4e246e5cb08b6
Mikey San <mikeyzx@gmail.com>
2020-12-28 21:07:45 +0000

    Added change path test for Windows systems

--- testingossys.py
+++ testingossys.py
@@ -4,6 +4,11 @@ import os
 # Building block to add more checks to our main script.
 if sys.platform.startswith('win'):
     print("You are on a Windows system")
+    print("Enter your full development path\n")
+    print("For example: C:\\Users\\phil4\\Documents\\development\n")
+    path = os.chdir(input(r'Enter path: '))
+    changeWorkingPath = os.getcwd()
+    print(changeWorkingPath)
 elif sys.platform.startswith('linux'):
     print("You are on Linux")
 elif sys.platform.startswith('darwin'):
